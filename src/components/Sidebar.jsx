790f49949b62b772fcf27c0cfd7273f6de4f7072
KJT9109 <kjt9109@naver.com>
2026-02-08 18:16:02 +0900

    feat: add sidebar of company portfolio menu

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,9 +9,9 @@ export default function Sidebar() {
 
       <a className="sideLink" href="#docs">Work Docs</a>
       <div className="sideSub">
-        <a className="sideSubLink" href="#docs-gitsn">Gitsn</a>
-        <a className="sideSubLink" href="#docs-nucare">Nucare</a>
-        <a className="sideSubLink" href="#docs-intellian">Intellian</a>
+        <a className="sideSubLink" href="#docs?company=gitsn">Gitsn</a>
+        <a className="sideSubLink" href="#docs?company=nucare">Nucare</a>
+        <a className="sideSubLink" href="#docs?company=intellian">Intellian</a>
       </div>
 
       <a className="sideLink" href="#blog">Blog</a>
