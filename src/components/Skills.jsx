0b1a9bfe2741625175bc49ce619d7e33e432e3d3
KJT9109 <kjt9109@naver.com>
2026-06-21 13:15:13 +0900

    add skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,12 +4,14 @@ export default function Skills() {
 	<h3>Skills</h3>
 
 	<ul>
-	<li><b>Application Platform</b> — Embedded Linux (Cortex-A), RTOS / Non-OS (Cortex-M)</li>
-	<li><b>Software Communication</b> — TCP/IP, REST API, Protocol Integration</li>
-	<li><b>Hardware Communication</b> — SPI, I2C, DCMI, Ethernet (lwIP)</li>
-	<li><b>Language</b> - C, C++</li>
-	<li><b>Manages</b> - Jira, Confluence, UML Diagram</li>
-	<li><b>Tools</b> - repo, git, svn</li>
+	<li><b>Application Platform</b>: Embedded Linux (Cortex-A), RTOS / baremetal (Cortex-M)</li>
+	<li><b>Application Maintenance</b>: Device Tree, U-boot, gdb</li>
+	<li><b>Software Communication</b>: TCP/IP, REST API, MQTT, Protocol Integration</li>
+	<li><b>Hardware Communication</b>: Serial(UART, SPI, I2C ..) DCMI, Ethernet(lwIP)</li>
+	<li><b>Hardware Debugging</b>: Circuit Analysis, w/ Oscilloscope</li>
+	<li><b>Language</b>: C, C++</li>
+	<li><b>Manages</b>: Jira, Confluence, UML Diagram</li>
+	<li><b>Tools</b>: repo, git, svn</li>
 	</ul>
 	</section>
 
